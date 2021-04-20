f3c00e196bd8fa5e553397c9ce4b9133879dc03b
Loi Tran <loit@ceterus.com>
2021-04-20 16:35:14 +0700

    update return type 2

--- lib/flutter_okta_sdk.dart
+++ lib/flutter_okta_sdk.dart
@@ -15,7 +15,7 @@ class OktaSDK {
     this.isInitialized = true;
   }
 
-  Future<Map<String, String?>> signIn() async {
+  Future<Map<String, String>> signIn() async {
     if (this.isInitialized == false) {
       throw Exception("Cannot sign in before initializing Okta SDK");
     }

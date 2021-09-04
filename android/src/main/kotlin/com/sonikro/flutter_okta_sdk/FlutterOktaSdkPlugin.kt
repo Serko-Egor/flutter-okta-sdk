08e2f2d4154e3a5cd0347fdb0f2c2651c5e2358b
Loi Tran <loit@ceterus.com>
2021-09-04 22:03:14 +0700

    fix for okta crash

--- android/src/main/kotlin/com/sonikro/flutter_okta_sdk/FlutterOktaSdkPlugin.kt
+++ android/src/main/kotlin/com/sonikro/flutter_okta_sdk/FlutterOktaSdkPlugin.kt
@@ -43,8 +43,17 @@ class FlutterOktaSdkPlugin : FlutterPlugin, MethodCallHandler,
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
-        OktaClient.getWebClient().handleActivityResult(requestCode, resultCode, data)
-        return PendingOperation.hasPendingOperation != null
+        try {
+            val client = OktaClient.getWebClient()
+            if (client != null) {
+                OktaClient.getWebClient().handleActivityResult(requestCode, resultCode, data)
+                return PendingOperation.hasPendingOperation != null
+            } else {
+                return false
+            }
+        } catch (ex: java.lang.Exception) {
+            return false
+        }
     }
 
     override fun onDetachedFromActivity() {
